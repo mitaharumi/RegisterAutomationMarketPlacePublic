822699c6bd58ff129e945e5cf7136a7a2355f3c3
mita <mitaharumi@gmail.com>
2021-02-09 21:34:08 -0300

    remove import actionchains

--- magazineLuiza.py
+++ magazineLuiza.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.keys import Keys
 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
 import time
 import rpa as r
 
